3e676667c30e17e7a464cd73524240de9d9b6b58
Tlotlego Mogapi <tlotlegomgp@gmail.com>
2020-07-15 18:18:45 +0200

    modified_the_api_call_to_call_100_items

--- index/views.py
+++ index/views.py
@@ -6,8 +6,10 @@ import os
 # Create your views here.
 
 def index_page(request):
+    context = {}
     if request.method == "POST":
         form = RegisterForm(request.POST)
+        context['form'] = form
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
@@ -16,10 +18,10 @@ def index_page(request):
             pass 
     else:
         form = RegisterForm()
+        context['form'] = form
         API_KEY = os.environ.get('NEWSAPI_KEY')
-        url = ('http://newsapi.org/v2/top-headlines?''country=za&''apiKey={}')
+        url = ('http://newsapi.org/v2/top-headlines?''country=za&''pageSize=100&''apiKey={}')
         response = requests.get(url.format(API_KEY)).json()
-            
-        print(response)
+        context['articles'] = response['articles']
 
-    return render(request, 'index/index.html', {'form' : form})
+    return render(request, 'index/index.html', context)
