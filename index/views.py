40714810c4f122f39f158f4c79de08078427068c
Tlotlego Mogapi <tlotlegomgp@gmail.com>
2020-07-15 17:24:05 +0200

    added_url_call_for_articles

--- index/views.py
+++ index/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from . forms import RegisterForm
+import requests
+import os
 
 # Create your views here.
 
 def index_page(request):
-    #If user has filled the email form and wants to post form
-	if request.method == "POST":
-		form = RegisterForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data["username"]
-			password = form.cleaned_data["password"]
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
 
-            #Process Create User
+        else:
+            pass 
+    else:
+        form = RegisterForm()
+        API_KEY = os.environ.get('NEWSAPI_KEY')
+        url = ('http://newsapi.org/v2/top-headlines?''country=za&''apiKey={}')
+        response = requests.get(url.format(API_KEY)).json()
+            
+        print(response)
 
-	#Else Present empty form to user
-	else:
-		form = RegisterForm()
-		
-	return render(request, 'index/index.html', {'form': form})
-	
+    return render(request, 'index/index.html', {'form' : form})
