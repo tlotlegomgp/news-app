d7f82ea74ae0204481056777b977e535a21155ee
Tlotlego Mogapi <tlotlegomgp@gmail.com>
2020-07-18 16:47:05 +0200

    changed settings

--- news_app/settings.py
+++ news_app/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#STATICFILES_STORAGE = 'whitenoise.storage.Compressed.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.Compressed.ManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
