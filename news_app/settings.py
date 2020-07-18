18ac044c0d6637fe0a4a98343bfe93599e8709cf
Tlotlego Mogapi <tlotlegomgp@gmail.com>
2020-07-18 16:16:30 +0200

    changed allowed hosts

--- news_app/settings.py
+++ news_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("NEWS_APP_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE") == 'False'
 
-ALLOWED_HOSTS = ['mzansinews.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
