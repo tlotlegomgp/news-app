1447b9744d3c8983e2e378e51b67fdc6e66fec69
Tlotlego Mogapi <tlotlegomgp@gmail.com>
2020-07-18 13:59:59 +0200

    changed_settings_timezone

--- news_app/settings.py
+++ news_app/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("NEWS_APP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_VALUE")
+DEBUG = TRUE
 
 ALLOWED_HOSTS = []
 
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'SAST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
