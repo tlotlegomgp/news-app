ac17bf656edef0e12338141bf336e2d27fef16aa
Tlotlego Mogapi <tlotlegomgp@gmail.com>
2020-07-18 16:14:23 +0200

    changed debug value

--- news_app/settings.py
+++ news_app/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("NEWS_APP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_VALUE")
+DEBUG = os.environ.get("DEBUG_VALUE") == 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mzansinews.herokuapp.com']
 
 
 # Application definition
